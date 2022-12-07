96d540f38b84008e4969634cbc19aede5b9b7aa9
Randy Cisneros <rlcisneros90@gmail.com>
2022-12-07 09:26:43 -0700

    Add destinations

--- friends.js
+++ friends.js
@@ -3,4 +3,9 @@ console.log(arrays[2]);
 console.log(arrays[4]);
 console.log(arrays[0]);
 
+var locals = ["NYC", "Hawaii", "CA", "Spain", "LA"]
+console.log(locals.shift())
+console.log(locals)
+
+
 
